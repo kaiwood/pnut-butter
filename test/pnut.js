02b98a72fb19c2b660836472af14b6ac37ff3417
Kai Wood <kwood@kwd.io>
2017-02-11 15:00:49 +0100

    Finish user lookup

--- test/pnut.js
+++ test/pnut.js
@@ -11,15 +11,20 @@ const pnut = require('../lib/pnut');
 describe('The pnut API wrapper', function () {
 
   before(function() {
-    let base = 'https://api.pnut.io';
+    let base = 'https://api.pnut.io/v0';
 
     nock(base)
-      .get('/v0')
+      .get('/posts/streams/global')
       .reply(200, {})
 
     nock(base)
-      .get('/v0/posts/streams/global')
-      .reply(200, {posts: 1})
+      .get('/users/1')
+      .reply(200, {})
+
+    nock(base)
+      .get('/users?ids=4,12,1000')
+      .reply(200, {})
+
   });
 
   after(function() {
@@ -27,6 +32,14 @@ describe('The pnut API wrapper', function () {
   });
 
   it('should get the global timeline', function() {;
-    return pnut.global().should.become({'posts': 1})
+    return pnut.global().should.become({})
   });
+
+  it('should be able to fetch a user by id', () => {
+    return pnut.user(1).should.become({});
+  });
+
+  it('should be able to fetch an array of users by their ids', () => {
+    return pnut.users([4, 12, 1000]).should.become({});
+  })
 });
