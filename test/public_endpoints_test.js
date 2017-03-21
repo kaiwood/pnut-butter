f625caf03ed390a9c0e9390cb49ddc1efee8c90e
Kai Wood <kwood@kwd.io>
2017-03-21 21:10:04 +0100

    Fix nesting of tests

--- test/public_endpoints_test.js
+++ test/public_endpoints_test.js
@@ -77,69 +77,67 @@ before(function () {
   nock(base)
     .patch('/somewhere')
     .reply(200, {})
-
-  after(function () {
-    nock.cleanAll();
-  });
-
-  /**
-   * User context
-   */
-  describe('In the context of users, it', () => {
-    it('should be able to fetch a user by id', () => {
-      return pnut.user(1).should.become({});
-    });
-
-    it('should be able to fetch an array of users by their ids', () => {
-      return pnut.users([4, 12, 1000]).should.become({});
-    });
-
-    it('should be able to fetch a users avatar', () => {
-      // TODO: Test should be more specific about the return values
-      expect(pnut.avatar(1)).to.be.fulfilled;
-    });
-
-    it('should be able to fetch a users cover', () => {
-      expect(pnut.cover('1')).to.be.fulfilled
-    })
-  });
-
-    /**
-   * Post context
-   */
-  describe('In the context of posts, it', () => {
-    it('should be able to fetch a post by id', () => {
-      return pnut.post(1).should.become({});
-    });
-
-    it('should be able to fetch an array of posts by their ids', () => {
-      return pnut.posts([4, 12, 1000]).should.become({});
-    });
-
-    it('should be able to fetch revisions of posts', () => {
-      return pnut.revisions(1).should.become({});
-    });
-
-    it('should be able to fetch mentions for a specific user', () => {
-      return pnut.mentions(1).should.become({});
-    });
-
-    it('should be able to fetch posts of a specific user', () => {
-      return pnut.postsFrom(1).should.become({});
-    });
-
-    it('should be able to fetch posts by tags', () => {
-      return pnut.postsTaggedWith('pnut-butter').should.become({});
-    });
-
-    it('should be able to fetch posts within a thread', () => {
-      return pnut.thread('1').should.become({});
-    });
-
-    it('should be able to fetch actions executed against a post', () => {
-      return pnut.actions('1').should.become({});
-    });
-  });
 });
 
+after(function () {
+  nock.cleanAll();
+});
 
+/**
+ * User context
+ */
+describe('In the context of users, it', () => {
+  it('should be able to fetch a user by id', () => {
+    return pnut.user(1).should.become({});
+  });
+
+  it('should be able to fetch an array of users by their ids', () => {
+    return pnut.users([4, 12, 1000]).should.become({});
+  });
+
+  it('should be able to fetch a users avatar', () => {
+    // TODO: Test should be more specific about the return values
+    expect(pnut.avatar(1)).to.be.fulfilled;
+  });
+
+  it('should be able to fetch a users cover', () => {
+    expect(pnut.cover('1')).to.be.fulfilled
+  })
+});
+
+/**
+* Post context
+*/
+describe('In the context of posts, it', () => {
+  it('should be able to fetch a post by id', () => {
+    return pnut.post(1).should.become({});
+  });
+
+  it('should be able to fetch an array of posts by their ids', () => {
+    return pnut.posts([4, 12, 1000]).should.become({});
+  });
+
+  it('should be able to fetch revisions of posts', () => {
+    return pnut.revisions(1).should.become({});
+  });
+
+  it('should be able to fetch mentions for a specific user', () => {
+    return pnut.mentions(1).should.become({});
+  });
+
+  it('should be able to fetch posts of a specific user', () => {
+    return pnut.postsFrom(1).should.become({});
+  });
+
+  it('should be able to fetch posts by tags', () => {
+    return pnut.postsTaggedWith('pnut-butter').should.become({});
+  });
+
+  it('should be able to fetch posts within a thread', () => {
+    return pnut.thread('1').should.become({});
+  });
+
+  it('should be able to fetch actions executed against a post', () => {
+    return pnut.actions('1').should.become({});
+  });
+});
