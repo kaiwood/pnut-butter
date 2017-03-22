24cdc6d77fbdb00a0b6ea2ef6d77162d3d74da82
Kai Wood <kwood@kwd.io>
2017-03-22 11:22:18 +0100

    Implement all endpoints for posts

--- test/posts_test.js
+++ test/posts_test.js
@@ -11,29 +11,63 @@ before(function () {
 
   nock(base)
     .get('/posts/1')
-    .reply(200, {})
+    .reply(200, {});
 
   nock(base)
     .get('/posts?ids=4,12,1000')
-    .reply(200, {})
+    .reply(200, {});
 
   nock(base)
     .get('/posts/1/revisions')
-    .reply(200, {})
+    .reply(200, {});
 
   nock(base)
     .get('/posts/tag/pnut-butter')
-    .reply(200, {})
+    .reply(200, {});
 
   nock(base)
     .get('/posts/1/thread')
-    .reply(200, {})
+    .reply(200, {});
 
   nock(base)
     .get('/posts/1/actions')
-    .reply(200, {})
+    .reply(200, {});
 
+  nock(base)
+    .get('/users/me/actions')
+    .reply(200, {});
 
+  nock(base)
+    .post('/posts', {text: 'An awesome post'})
+    .reply(200, {});
+
+  nock(base)
+    .put('/posts/1000', {text: 'A revised, but still awesome post'})
+    .reply(200, {});
+
+  nock(base)
+    .delete('/posts/1000')
+    .reply(200, {});
+
+  nock(base)
+    .put('/posts/1000/repost')
+    .reply(200, {});
+
+  nock(base)
+    .delete('/posts/1000/repost')
+    .reply(200, {});
+
+  nock(base)
+    .get('/users/1/bookmarks')
+    .reply(200, {});
+
+  nock(base)
+    .put('/posts/1000/bookmark')
+    .reply(200, {});
+
+  nock(base)
+    .delete('/posts/1000/bookmark')
+    .reply(200, {});
 });
 
 after(function () {
@@ -71,6 +105,42 @@ describe('Post endpoints', () => {
 
   it('should be able to fetch actions executed against a post', () => {
     return pnut.actions('1').should.become({});
+  });
+
+  it('should be able to fetch actions for the posts of the authenticated user', () => {
+    return pnut.usersActions().should.become({});
+  });
+
+  it('should be able to create a new post', () => {
+    return pnut.createPost("An awesome post").should.become({});
+  });
+
+  it('should be able to edit a post', () => {
+    return pnut.updatePost(1000, "A revised, but still awesome post").should.become({});
+  });
+
+  it('should be able to delete a post', () => {
+    return pnut.deletePost(1000).should.become({});
+  });
+
+  it('should be able to repost something', () => {
+    return pnut.repost(1000).should.become({});
+  });
+
+  it('should be able to delete a repost', () => {
+    return pnut.deleteRepost(1000).should.become({});
+  });
+
+  it('should be able to fetch a list of posts bookmarked by a user', () => {
+    return pnut.bookmarks(1).should.become({});
+  });
+
+  it('should be able to bookmark a post', () => {
+    return pnut.bookmark(1000).should.become({});
+  });
+
+  it('should be able to delete a bookmark', () => {
+    return pnut.deleteBookmark(1000).should.become({});
   });
 
 });
