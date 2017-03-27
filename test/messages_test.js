487cd14b110de1497be734d4328e4bcdcc3aedab
Kai Wood <kwood@kwd.io>
2017-03-27 21:17:16 +0200

    Implement message related endpoints

--- test/messages_test.js
+++ test/messages_test.js
@@ -12,6 +12,48 @@ before(function () {
   nock(base)
     .get('/somewhere')
     .reply(200, {});
+
+  nock(base)
+    .get('/channels/1/messages/1')
+    .reply(200, {});
+
+  nock(base)
+    .get('/channels/1/messages/1/thread')
+    .reply(200, {});
+
+  nock(base)
+    .get('/channels/messages?ids=1,2,3')
+    .reply(200, {});
+
+  nock(base)
+    .get('/channels/1/messages')
+    .reply(200, {});
+
+  nock(base)
+    .get('/users/me/messages')
+    .reply(200, {});
+
+  nock(base)
+    .post('/channels/1/messages', {
+      text: 'An important message'
+    })
+    .reply(200, {});
+
+  nock(base)
+    .delete('/channels/1/messages/1')
+    .reply(200, {});
+
+  nock(base)
+    .get('/channels/1/sticky_messages')
+    .reply(200, {});
+
+  nock(base)
+    .put('/channels/1/messages/1/sticky')
+    .reply(200, {});
+
+  nock(base)
+    .delete('/channels/1/messages/1/sticky')
+    .reply(200, {});
 });
 
 after(function () {
@@ -19,16 +61,43 @@ after(function () {
 });
 
 describe('Messages', () => {
-  it('should be able to retrieve a message from a channels (pnut.message)');
-  it('should be able to retrieve a thread of messages (pnut.channelThread)');
-  it('should be able to find specified message (pnut.findMessages)');
-  it('should be able to retrieve messages from a channel (pnut.messages)');
-  it('should be able to retrieve a list of the authenticated users messages (pnut.usersMessages)');
+  it('should be able to retrieve a message from a channel', () => {
+    pnut.message(1, 1).should.become({});
+  });
 
-  it('should be able to send a message (pnut.createMessage)');
-  it('should be able to delete a message (pnut.deleteMessage)');
+  it('should be able to retrieve a thread of messages', () => {
+    pnut.messageThread(1, 1).should.become({});
+  });
 
-  it('should be able to retrieve a list of sticky messages (pnut.stickies)');
-  it('should be able to create a sticky (pnut.sticky)');
-  it('should be able to delete a sticky (pnut.unsticky)');
+  it('should be able to find specified message', () => {
+    pnut.messages(1, 2, 3).should.become({});
+  });
+
+  it('should be able to retrieve messages from a channel', () => {
+    pnut.channelMessages(1).should.become({});
+  });
+
+  it('should be able to retrieve a list of the authenticated users messages (pnut.usersMessages)', () => {
+    pnut.personalMessages().should.become({});
+  });
+
+  it('should be able to send a message', () => {
+    pnut.createMessage(1, "An important message").should.become({});
+  });
+
+  it('should be able to delete a message', () => {
+    pnut.deleteMessage(1, 1).should.become({});
+  });
+
+  it('should be able to retrieve a list of sticky messages (pnut.stickies)', () => {
+    pnut.stickies(1).should.become({});
+  });
+
+  it('should be able to create a sticky', () => {
+    pnut.sticky(1, 1).should.become({});
+  });
+
+  it('should be able to delete a sticky', () => {
+    pnut.unsticky(1, 1).should.become({});
+  });
 });
