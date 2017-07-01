6418f42674927064269629d7434c2b4867899e36
Kai Wood <kwood@kwd.io>
2017-07-01 11:09:22 +0200

    Add stream marker support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.10.0
+
+- Stream marker support
+
 ## 0.9.0
 
 - Add "Missed Conversations" stream
---
+++ test/stream_markers_test.js
@@ -0,0 +1,52 @@
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+const nock = require("nock");
+chai.use(chaiAsPromised);
+chai.should();
+
+const pnut = require("../lib/pnut");
+
+before(function() {
+  let base = "https://api.pnut.io/v0";
+
+  nock(base)
+    .post("/markers", [
+      {
+        name: "channel:18",
+        id: 15100
+      }
+    ])
+    .reply(200, {});
+
+  nock(base)
+    .post("/markers", [
+      {
+        name: "channel:18",
+        id: 15100
+      },
+      {
+        name: "channel:19",
+        id: 15101
+      }
+    ])
+    .reply(200, {});
+});
+
+after(function() {
+  nock.cleanAll();
+});
+
+describe("Stream markers", () => {
+  it("should be able to update a single stream marker", () => {
+    return pnut.marker("channel:18", 15100).should.become({});
+  });
+
+  it("should be able to update a multiple stream markers", () => {
+    return pnut
+      .markers([
+        { name: "channel:18", id: 15100 },
+        { name: "channel:19", id: 15101 }
+      ])
+      .should.become({});
+  });
+});
