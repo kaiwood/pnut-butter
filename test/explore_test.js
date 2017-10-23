b393a051b8bfa15d2225d363361d5dce7e60db90
Kai Wood <kwood@kwd.io>
2017-10-23 17:33:52 +0200

    Add 'explore' endpoint

--- test/explore_test.js
+++ test/explore_test.js
@@ -9,13 +9,28 @@ const pnut = require("../lib/pnut");
 before(function() {
   let base = "https://api.pnut.io/v0";
 
-  nock(base).get("/posts/streams/explore/conversations").reply(200, {});
-  nock(base).get("/posts/streams/explore/photos").reply(200, {});
-  nock(base).get("/posts/streams/explore/trending").reply(200, {});
-  nock(base).get("/posts/streams/explore/missed_conversations").reply(200, {});
+  nock(base)
+    .get("/posts/streams/explore")
+    .reply(200, {});
+  nock(base)
+    .get("/posts/streams/explore/conversations")
+    .reply(200, {});
+  nock(base)
+    .get("/posts/streams/explore/photos")
+    .reply(200, {});
+  nock(base)
+    .get("/posts/streams/explore/trending")
+    .reply(200, {});
+  nock(base)
+    .get("/posts/streams/explore/missed_conversations")
+    .reply(200, {});
 });
 
 describe("Explore", () => {
+  it("should be able to fetch the explore endpoint", () => {
+    return pnut.explore().should.become({});
+  });
+
   it("should be able to fetch the 'explore conversations' stream", () => {
     return pnut.conversations().should.become({});
   });
